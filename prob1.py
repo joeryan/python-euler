f0da17bd37aee822281c8c40d12345e979d6d883
Joe Ryan <joe@clanryan.us>
2015-05-25 21:59:43 -0400

    initial project euler problem 1, add numbers that are multiples of 3 and 5

---
+++ prob1.py
@@ -0,0 +1,9 @@
+
+class Prob1():
+    def __init__(self, maxNum):
+        self.maxNum = maxNum
+
+    def answer(self):
+        print "No method yet"
+        print "The maximum number range is: %d" % self.maxNum
+        return self.maxNum
