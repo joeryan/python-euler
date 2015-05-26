d5d658ac04a52cedcce2f89098ec31afacf4d994
Joe Ryan <joe@clanryan.us>
2015-05-25 22:31:34 -0400

    add final test to test1.py, add setMax method to prob1 class

--- prob1.py
+++ prob1.py
@@ -1,9 +1,17 @@
 
 class Prob1():
-    def __init__(self, maxNum):
+    def __init__(self):
+        self.maxNum = 0
+
+    def setMax(self, maxNum):
         self.maxNum = maxNum
+        print "The maximum number range is: %d" % self.maxNum
 
     def answer(self):
-        print "No method yet"
-        print "The maximum number range is: %d" % self.maxNum
-        return self.maxNum
+        total = 0;
+        for number in range(1,self.maxNum):
+            if number % 3 == 0:
+                total += number
+            elif number % 5 == 0:
+                total += number
+        return total 
--- test1.py
+++ test1.py
@@ -2,12 +2,21 @@ import unittest
 import prob1
 
 class TestProb1(unittest.TestCase):
-    def runTest(self):
-        problem = prob1.Prob1(10)
-        assert problem.answer() == (23), "incorrect answer %d" % problem.answer()
+    def setUp(self):
+        self.prob1 = prob1.Prob1()
 
-testCase = TestProb1()
+    def testInitial(self):
+        self.prob1.setMax(10)
+        assert self.prob1.answer() == (23), "incorrect answer %d" % prob1.answer()
+
+    def testFinal(self):
+        self.prob1.setMax(1000)
+        assert self.prob1.answer() == (100), "incorrect answer %d" % self.prob1.answer()
+
+prob1TestSuite = unittest.TestSuite()
+
+prob1TestSuite.addTest(TestProb1("testInitial"))
+prob1TestSuite.addTest(TestProb1("testFinal"))
 
 runner = unittest.TextTestRunner()
-runner.run(testCase)
-
+runner.run(prob1TestSuite)
