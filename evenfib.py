3a7d04b9cabf974bd2b5f47bf20bb02ce964bb3f
Joe Ryan <joe@clanryan.us>
2015-05-26 23:14:27 -0400

    final rework of evenfib.py and edit of answer for unit tests to prove project euler correctness

--- evenfib.py
+++ evenfib.py
@@ -1,20 +1,23 @@
+from sys import maxint
+
 class EvenFib():
-	def __init__(self):
-		self.terms = -1 
-		self.maxVal = -1
+    
+    def __init__(self):
+	self.terms = maxint 
+	self.maxVal = maxint 
 		
-	def setTerms(self, terms):
-		self.terms = terms
+    def setTerms(self, terms):
+	self.terms = terms
 
-	def setMaxVal(self, maxVal):
-		self.maxVal = maxVal
+    def setMaxVal(self, maxVal):
+	self.maxVal = maxVal
 
-	def evenFib(self):
-		sumEvenFib = 0
-		curr, follow = 1, 2
-		while (sumEvenFib < self.maxVal or self.maxVal
-		for num in range(1, terms):
-			if curr % 2 == 0:
-				sumEvenFib += curr
-			curr, follow = follow, curr + follow
-		return sumEvenFib
+    def evenFibSum(self):
+	sumEvenFib, terms = 0, 1
+	curr, follow = 1, 2
+        while (sumEvenFib < self.maxVal and terms < self.terms):
+            if curr % 2 == 0:
+                sumEvenFib += curr
+            curr, follow = follow, curr + follow
+            terms += 1
+        return sumEvenFib
--- test2.py
+++ test2.py
@@ -3,15 +3,15 @@ import evenfib
 
 class TestEvenFib(unittest.TestCase):
     def setUp(self):
-        self.evenFib = evenFib.evenFib()
+        self.evenFib = evenfib.EvenFib()
 
     def testInitial(self):
         self.evenFib.setTerms(10)
-        assert self.evenFib.answer() == (44), "incorrect answer %d" % evenFib.answer()
+        assert self.evenFib.evenFibSum() == (44), "incorrect answer %d" % evenFib.evenFibSum()
 
     def testFinal(self):
         self.evenFib.setMaxVal(4000000)
-        assert self.evenFib.answer() == (233168), "incorrect answer %d" % self.evenFib.answer()
+        assert self.evenFib.evenFibSum() == (4613732), "incorrect answer %d" % self.evenFib.evenFibSum()
 
 evenFibTestSuite = unittest.TestSuite()
 
