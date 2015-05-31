b80776bcddc956e83ca810adc699cb48772bb474
Joe Ryan <joe@clanryan.us>
2015-05-31 19:35:14 -0400

    algorithm using reduce, long runtime

--- lgprime.py
+++ lgprime.py
@@ -4,10 +4,14 @@
 class LgPrime():
     def __init__(self):
         self.num = 0
+        self.primes = []
 
     def setNumber(self, num):
         self.num = num
 
     def lgPrimeNum(self):
-        return 29
+        self.primes = set(reduce(list.__add__,
+            ([i, self.num//i] for i in range(1, int(self.num**0.5) + 1) if self.num % 1 == 0)))
+        return max(self.primes)
+
 # determine the largest
