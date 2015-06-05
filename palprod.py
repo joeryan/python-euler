1f0774e919ad681befef114e992281d653f9d8dd
Joe Ryan <joe@clanryan.us>
2015-06-04 23:01:12 -0400

    refactor palprod.py to calculate an answer

--- palprod.py
+++ palprod.py
@@ -1,13 +1,16 @@
 class PalProd():
     def __init__(self):
         self.maxProd = 0
-        self.digits = 1
+        self.maxNum = 9
 
     def setDigits(self, dig):
-        self.digits = dig
-
+        self.maxNum = pow(10, dig) 
+        
     def calcMaxProd(self):
-        self.maxProd = 9009
+        for i in range(1, self.maxNum):
+            for j in range(1, self.maxNum):
+                if str(i*j)  == str(i*j)[::-1] and self.maxProd < i*j:
+                    self.maxProd = i*j
 
     def getMaxProd(self):
         return self.maxProd
