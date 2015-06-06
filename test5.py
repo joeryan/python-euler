6fa9a013ed39c5ce7431ffed7bb2070dd7897350
Joe Ryan <joe@clanryan.us>
2015-06-05 22:45:42 -0400

    solved with brute force lcm algorithm

--- test5.py
+++ test5.py
@@ -13,7 +13,7 @@ class TestSmallMult(unittest.TestCase):
         assert self.smallMult.getMult() == (2520), "incorrect answer %d" % self.smallMult.getMult()
 
     def testFinal(self):
-        self.smallMult.setDigits(10)
+        self.smallMult.setDigits(20)
         self.smallMult.calcSmallMult()
         assert self.smallMult.getMult() == (0), "incorrect answer %d" % self.smallMult.getMult()
 
